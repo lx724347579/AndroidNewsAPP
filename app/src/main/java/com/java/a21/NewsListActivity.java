f7eff6c46eaf2c432e8ff12cda01f08cb6237413
cdogemaru <cdogemaru@outlook.com>
2017-09-13 21:02:41 +0800

    cnbb

--- app/src/main/java/com/java/a21/NewsListActivity.java
+++ app/src/main/java/com/java/a21/NewsListActivity.java
@@ -1,6 +1,8 @@
 package com.java.a21;
 
 import android.content.Context;
+import android.content.Intent;
+import android.graphics.pdf.PdfDocument;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
@@ -15,8 +17,12 @@ import android.support.design.widget.TabLayout;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.view.ViewPager;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 
 public class NewsListActivity extends AppCompatActivity {
@@ -25,21 +31,30 @@ public class NewsListActivity extends AppCompatActivity {
 
     private ViewPager viewPager;
 
+    private ImageButton addTabButton;
+
     private TabLayout tabLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_tab);
-//        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-//        setSupportActionBar(toolbar);
-
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_news_list);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        addTabButton = (ImageButton) findViewById(R.id.addTabButton);
         setSupportActionBar(toolbar);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+
+
+        addTabButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(NewsListActivity.this, TypeActivity.class);
+                intent.putExtra("chosen", pagerAdapter.chosen);
+                intent.putExtra("typename", pagerAdapter.tabTitles);
+                startActivityForResult(intent, 1500);
+            }
+        });
 
         pagerAdapter = new SimpleFragmentPagerAdapter(getSupportFragmentManager(), this);
         viewPager = (ViewPager) findViewById(R.id.viewpager);
@@ -47,6 +62,16 @@ public class NewsListActivity extends AppCompatActivity {
         tabLayout = (TabLayout) findViewById(R.id.sliding_tabs);
         tabLayout.setupWithViewPager(viewPager);
         tabLayout.setTabMode(TabLayout.MODE_SCROLLABLE);
+    }
+
+    //重写   处理返回信息的监听（回调方法）
+    //onActivityResult通用监听  监听所有返回信息的
+    //必须要有requestCode区分有哪个请求返回的
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        pagerAdapter.chosen = data.getBooleanArrayExtra("chosen");
+        pagerAdapter.refresh();
     }
 
     @Override
@@ -57,28 +82,61 @@ public class NewsListActivity extends AppCompatActivity {
 
     public class SimpleFragmentPagerAdapter extends FragmentPagerAdapter {
 
-        final int PAGE_COUNT = 3;
-        private String tabTitles[] = new String[]{"tab1","tab2","tab3"};
+        int pagecount = 12;
+        private String tabTitles[] = new String[]{
+                "科技","教育","军事","国内","社会","文化","汽车","国际","体育","财经","健康","娱乐"};
         private Context context;
 
+        public boolean[] chosen;
         public SimpleFragmentPagerAdapter(FragmentManager fm,Context context) {
             super(fm);
+
             this.context = context;
+            chosen = new boolean[12];
+            for(int i = 0; i < 12; i ++) {
+                chosen[i] = true;
+            }
+        }
+        public void refresh() {
+            int tmp = 0;
+            for(int i = 0; i < 12; i ++) {
+                if(chosen[i] == true) {
+                    tmp ++;
+                }
+            }
+            pagecount = tmp;
+            notifyDataSetChanged();
+        }
+
+        public String gettitle(int position) {
+            int cnt = 0;
+            int result = 0;
+            for(int i = 0; i < 12; i ++) {
+                if(chosen[i] == true) {
+                    cnt ++;
+                    result = i;
+                }
+                if(cnt == position + 1) {
+                    break;
+                }
+            }
+            return tabTitles[result];
         }
 
         @Override
         public Fragment getItem(int position) {
-            return PageFragment.newInstance(tabTitles[position]);
+            return PageFragment.newInstance(gettitle(position));
+            //return PageFragment.newInstance(tabTitles[position]);
         }
 
         @Override
         public int getCount() {
-            return PAGE_COUNT;
+            return pagecount;
         }
 
         @Override
         public CharSequence getPageTitle(int position) {
-            return tabTitles[position];
+            return gettitle(position);
         }
     }
 }
--- app/src/main/java/com/java/a21/TypeActivity.java
+++ app/src/main/java/com/java/a21/TypeActivity.java
@@ -1,29 +1,65 @@
 package com.java.a21;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.IntegerRes;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
 
 public class TypeActivity extends AppCompatActivity {
-
+    Button button[] = new Button[12];
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Intent intent = getIntent();
+        final boolean[] chosen = intent.getBooleanArrayExtra("chosen");
+        final String[] typename = intent.getStringArrayExtra("typename");
+        Integer[] id = {R.id.button0,R.id.button1,R.id.button2,R.id.button3,R.id.button4,R.id.button5,R.id.button6,R.id.button7,R.id.button8,R.id.button9,R.id.button10,R.id.button11};
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_type);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener(){
+            public void onClick(View view){
+                Intent rintent = new Intent();
+                rintent.putExtra("chosen", chosen);
+                setResult(1500, rintent);
+                finish();
             }
         });
+
+
+        for(int i = 0; i < 12; i ++) {
+            final int tmp = i;
+            button[i] = (Button) findViewById(id[i]);
+            button[i].setText(typename[i]);
+            if(chosen[i] == true) {
+                button[i].setBackgroundColor(getResources().getColor(R.color.colorPrimary));
+            }
+            else {
+                button[i].setBackgroundColor(getResources().getColor(R.color.colorUnchosen));
+            }
+
+            button[i].setOnClickListener(new View.OnClickListener() {
+                @Override
+
+                public void onClick(View v) {
+                    if(chosen[tmp] == false) {
+                        button[tmp].setBackgroundColor(getResources().getColor(R.color.colorPrimary));
+                        chosen[tmp] = true;
+                    }
+                    else {
+                        button[tmp].setBackgroundColor(getResources().getColor(R.color.colorUnchosen));
+                        chosen[tmp] = false;
+                    }
+                }
+            });
+        }
     }
 
 }
