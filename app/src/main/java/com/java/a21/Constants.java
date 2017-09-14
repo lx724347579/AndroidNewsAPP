bd75ff86b74093fac1918b2c8df58077745a8d04
mektpoy <471811570@qq.com>
2017-09-14 22:50:22 +0800

    bug fixed

--- app/src/main/java/com/java/a21/Constants.java
+++ app/src/main/java/com/java/a21/Constants.java
@@ -53,8 +53,7 @@ public interface Constants {
      * 关于 Scope 概念及注意事项，请查看：http://open.weibo.com/wiki/Scope
      */
     public static final String SCOPE =
-            "email,direct_messages_read,direct_messages_write,"
-            + "friendships_groups_read,friendships_groups_write,statuses_to_me_read,"
-            + "follow_app_official_microblog," + "invitation_write";
+            "email,direct_messages_read,direct_messages_write"
+            ;
    // public static final String SCOPE = null;
 }
